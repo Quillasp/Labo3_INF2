b92c52b8e33ea16b0526a1953a790994c570e199
Emmanuel Janssens <emmanuel.janssens@heig-vd.ch>
2019-03-25 16:38:29 +0100

    Ajoute des commentaires de la classe POINT

--- Point.h
+++ Point.h
@@ -1,3 +1,13 @@
+
+/**
+ * @file Point.h
+ * @author Janssens Emmanuel
+ * @brief  Coordonnées dans le plan réel
+ * @date 2019-03-24
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #ifndef POINT_H
 #define POINT_H
 
@@ -17,18 +27,31 @@ class Point
     float distance()const ;
 
 
+    /**
+     * @brief Surcharge des opérateurs de comparaisons
+     * 
+     * @param p 
+     * @return true 
+     * @return false 
+     */
     bool operator>(const Point& p)const;
     bool operator<(const Point& p)const;
     bool operator>=(const Point& p)const;
     bool operator<=(const Point& p)const;
 
-
+    /**
+     * @brief Surcharge de l'opérateur de flux
+     * 
+     * @param os flux dans lequel il faut écrire
+     * @param point Point que l'on veut afficher
+     * @return std::ostream& 
+     */
     friend std::ostream& operator<<(std::ostream& os, const Point& point);
     
     private:
 
     /**
-     * Coordonnées dews points
+     * Coordonnées des points
      * */
     int x;
     int y;
